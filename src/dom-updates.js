1541c0aef104e3ffc0e069ec335797d999e45bb7
Eric Rabun <errabun@gmail.com>
2021-04-25 14:10:26 -0600

    Fix spacing/styling of trip cards

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -15,7 +15,18 @@ const domUpdates = {
     element.innerHTML = 'You spent $' + traveler.calcCurrentYearSpending(allDestinations) + ' this year on trips'
   },
 
-  addTripCardToDom(myTrips, element, allDestinations) {
+  addTripCardToDom(myTrips, allDestinations, element) {
+    // let element;
+    // myTrips.filter(trip => {
+    //   console.log(trip.status)
+    //   if (trip.status === 'pending') {
+    //     element = pendingTrips;
+    //   } else if (new Date(trip.date) > Date.now() && trip.status === 'approved') {
+    //     element = upcomingTrips;
+    //   } else {
+    //     return element = pastTrips;
+    //   }
+    // })
     return myTrips.map(trip => {
       return allDestinations.destinations.find(destination => {
         if (trip.destinationID === destination.id) {
@@ -23,25 +34,31 @@ const domUpdates = {
               <article class="destination-card" id=${destination.id}>
                 <h3>${destination.destination}</h3>
                 <p>${trip.date} for ${trip.duration} days</p>
+                <p>Trip Status: ${trip.status}</p>
                 <div class="card-photo-container">
                   <img src=${destination.image} class="card-photo-preview" alt="${destination.alt}" height="300px">
                 </div>
               </article>`
-          return element.insertAdjacentHTML("afterend", cardHtml);
+          return element.insertAdjacentHTML("afterbegin", cardHtml);
         }
       })
     })
   },
 
   bookNewTrip(destinations) {
-    const destSelect = document.querySelector('.destination-input');
-    destSelect.innerHTML =
-    `<label for="destination-list">Where to:</label>
-    <select aria-label="select-destination" class="destination-list" name="destination-list">
-    ${destinations.map(destination => {
+    const destinationSelect = document.querySelector('.destination-list');
+    destinationSelect.innerHTML =
+    `${destinations.map(destination => {
       return `<option value="${destination.id}">${destination.destination}</option>`
     })}`
   },
+
+  displayEstimateCost(element, costPerPerson, estimatedTotalCost, totalPlusFees) {
+    return element.innerHTML = `
+      <p class="estimate-per-person">Cost Per Person: ${costPerPerson}</p>
+      <p class="estimate-all">Total All Travelers: ${estimatedTotalCost}</p>
+      <p class="estimate-fees">Total + Agent Fees: ${totalPlusFees}</p>`
+  }
 }
 
 export default domUpdates;
--- src/index.js
+++ src/index.js
@@ -9,12 +9,21 @@ import datepicker from 'js-datepicker';
 
 let traveler, allTrips, allDestinations;
 
+const tripCardsGrid = document.querySelector('.cards-wrapper');
+const tripCardsSection = document.querySelector('.trip-cards');
+const departDay = document.querySelector('.start-date');
+const destinationSelect = document.querySelector('.destination-list');
+const departDaySelect = document.querySelector('#startDate');
+const tripDurationSelect = document.querySelector('#duration');
+const numTravelersSelect = document.querySelector('#numTravelers');
+const bookTripBtn = document.querySelector('.book-trip-btn');
+const estimateTripBtn = document.querySelector('.estimate-trip');
+const estimateDOMPointer = document.querySelector('.display-estimates');
 
 window.addEventListener('load', onStart)
+bookTripBtn.addEventListener('click', postTrip)
+estimateTripBtn.addEventListener('click', showEstimate)
 
-const pastTrips = document.querySelector('.past-title')
-const departDay = document.querySelector('.start-date')
-const returnDay = document.querySelector('.end-date')
 
 function onStart() {
   loadAPIs(2)
@@ -23,18 +32,52 @@ function onStart() {
     allTrips = new TripRepo(allData.getAllTrips);
     allDestinations = allData.getAllDestinations;
     allTrips.findTripsByID(traveler);
-    domUpdates.greetUser(traveler);
-    domUpdates.displayPrevYrSpending(traveler, allDestinations.destinations);
-    domUpdates.displayCurrentYrSpending(traveler, allDestinations.destinations)
-    domUpdates.addTripCardToDom(traveler.myTrips, pastTrips, allDestinations)
-    domUpdates.bookNewTrip(allDestinations.destinations); 
+    displayStartDOM();
   })
 }
 
-function showDestinationOptions(destinations) {
-
+function displayStartDOM () {
+  domUpdates.greetUser(traveler);
+  domUpdates.displayPrevYrSpending(traveler, allDestinations.destinations);
+  domUpdates.displayCurrentYrSpending(traveler, allDestinations.destinations)
+  domUpdates.addTripCardToDom(traveler.myTrips, allDestinations, tripCardsGrid)
+  domUpdates.bookNewTrip(allDestinations.destinations);
 }
 
-function bookNewTrip() {
+function postTrip() {
+  event.preventDefault();
+  if ( new Date(departDaySelect.value) > Date.now() ) {
+    let newTrip = fetch("http://localhost:3001/api/v1/trips", {
+      method: 'POST',
+      body: JSON.stringify({
+        "id": allTrips.length + 1,
+        "userID": 2,
+        "destinationID": parseInt(destinationSelect.value),
+        "travelers": numTravelersSelect.value,
+        "date": formatDate(departDaySelect.value),
+        "duration": tripDurationSelect.value,
+        "status": 'pending',
+        "suggestedActivities": []
+      }),
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err.message))
+  }
+}
 
+function formatDate(dateInput) {
+  const dateParts = dateInput.split('-')
+  return dateParts.join('/')
+}
+
+function showEstimate() {
+  event.preventDefault();
+  estimateDOMPointer.innerHTML = '';
+  const getDestination = allDestinations.destinations.find(destination => destination.id === parseInt(destinationSelect.value));
+  const costPerPerson = (getDestination.estimatedLodgingCostPerDay * tripDurationSelect.value) + getDestination.estimatedFlightCostPerPerson;
+  const estimatedTotalCost = costPerPerson * numTravelersSelect.value;
+  const totalPlusFees = parseInt(estimatedTotalCost * 1.1).toFixed(2);
+  domUpdates.displayEstimateCost(estimateDOMPointer, costPerPerson, estimatedTotalCost, totalPlusFees);
 }
